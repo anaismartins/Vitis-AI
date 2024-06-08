bd456d53ccb48b01972644a1606322b0e24dabd2
ana <anaisabelmartins@tecnico.ulisboa.pt>
2024-06-08 15:35:34 +0200

    feat: Add dependencies and set timezone in Vitis AI Dockerfile

--- docker/apptainer_build.sh
+++ docker/apptainer_build.sh
@@ -29,6 +29,17 @@ From: nvidia/cuda:11.8.0-cudnn8-devel-ubuntu20.04
         python3-pip \
         libopencv-dev \
         tzdata \
+        lsb-release \
+        dkms \
+        udev \
+        ocl-icd-opencl-dev \
+        uuid-dev \
+        libboost-filesystem1.71.0 \
+        libboost-program-options1.71.0 \
+        libboost-serialization-dev \
+        libboost-system-dev \
+        libboost-filesystem-dev \
+        libboost-thread-dev \
         && rm -rf /var/lib/apt/lists/*
 
     # Set the timezone
